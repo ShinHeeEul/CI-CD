0c6f5d7bb4feacbeeb664b28f4c327d071331fd7
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2023-07-07 00:51:32 +0900

    refactor: ci 테스트용

--- mytest/src/main/java/org/example/Main.java
+++ mytest/src/main/java/org/example/Main.java
@@ -5,8 +5,5 @@ public class Main {
         System.out.println("Hello world!");
         Member member = new Member("희을", "010-5900-7430");
         System.out.println(member.getName() + " : " + member.getPhone());
-
-        Member member1 = new Member("재현", "몰루");
-        System.out.println(member1.getName() + " : " + member1.getPhone());
     }
 }
