fc604d06018225d1966d46a093aae02f1859f28a
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2023-07-07 00:11:55 +0900

    feat: 프로젝트 생성

---
+++ mytest/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Member member = new Member("희을", "010-5900-7430");
+        System.out.println(member.getName() + " : " + member.getPhone());
+    }
+}
