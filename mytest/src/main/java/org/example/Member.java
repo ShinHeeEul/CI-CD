0503bbdbd3c82e999e280b2988eadfe4369ea1a9
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2023-07-07 00:12:42 +0900

    feat: Member 클래스 생성

---
+++ mytest/src/main/java/org/example/Member.java
@@ -0,0 +1,22 @@
+package org.example;
+
+
+
+public class Member {
+
+    private String name;
+    private String phone;
+
+    public Member(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
