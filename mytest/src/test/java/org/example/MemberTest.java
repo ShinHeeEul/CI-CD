4b46a606c1b0e0a3a758c6836025913458dad666
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2023-07-07 00:12:30 +0900

    test: Member 테스트 생성

---
+++ mytest/src/test/java/org/example/MemberTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MemberTest {
+
+    private Member member;
+    @Test
+    @DisplayName("검증")
+    void getName() {
+        member = new Member("희을", "010010");
+
+        Assertions.assertEquals(member.getName(), "희을");
+        Assertions.assertEquals(member.getPhone(), "010010");
+    }
+}
